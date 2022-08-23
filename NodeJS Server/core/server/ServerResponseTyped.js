c82847ded6100be3ed797ee7d623a37e9ff85e4f
Nikita Chekun <chekun2004@gmail.com>
2022-08-23 20:26:31 +0300

    Setup typescript.

--- NodeJS Server/core/server/ServerResponseTyped.js
+++
@@ -1,7 +0,0 @@
-// noinspection JSUnusedGlobalSymbols
-
-module.exports = function ServerResponseTyped(type, code, message) {
-    this.type = type;
-    this.code = code;
-    this.message = message;
-}
