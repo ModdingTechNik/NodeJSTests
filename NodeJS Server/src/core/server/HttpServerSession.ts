07568484a4ee71c506842df67467b1d7be46384d
Nikita Chekun <chekun2004@gmail.com>
2022-08-24 21:32:20 +0300

    Cleanup.

--- NodeJS Server/src/core/server/HttpServerSession.ts
+++ NodeJS Server/src/core/server/HttpServerSession.ts
@@ -2,9 +2,9 @@ import {IncomingMessage, OutgoingHttpHeader, ServerResponse} from "http";
 import {ServerJsonResponse, ServerRestResponseType} from "../api/ServerRestResponse";
 
 export default class HttpServerSession {
-    private readonly request: IncomingMessage;
-    private readonly response: ServerResponse;
-    private readonly globalHeader: OutgoingHttpHeader[];
+    private readonly request: IncomingMessage
+    private readonly response: ServerResponse
+    private readonly globalHeader: OutgoingHttpHeader[]
 
     public constructor(request: IncomingMessage, response: ServerResponse, globalHeaders : OutgoingHttpHeader[]) {
         this.request = request;
